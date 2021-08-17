fcd4cbbb9a82d2aba810313b97a46f192f7b969a
Torsten Hellwig <torsten.hellwig93@gmail.com>
2021-08-17 08:26:17 +0200

    New Net: nn-3cc4c71e74b4.nnue
    Bench: 4721844

--- src/evaluate.h
+++ src/evaluate.h
@@ -39,7 +39,7 @@ namespace Eval {
   // The default net name MUST follow the format nn-[SHA256 first 12 digits].nnue
   // for the build process (profile-build and fishtest) to work. Do not change the
   // name of the macro, as it is used in the Makefile.
-  #define EvalFileDefaultName   "nn-e8321e467bf6.nnue"
+  #define EvalFileDefaultName   "nn-3cc4c71e74b4.nnue"
 
   namespace NNUE {
 
