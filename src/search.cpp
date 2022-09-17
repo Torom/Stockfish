8d3fadb8859d8bba4e2fdb4849550aa407a47bf3
Torsten Hellwig <torsten.hellwig93@gmail.com>
2022-09-17 09:41:47 +0200

    Use less time when hash is full bench 4326572

--- src/search.cpp
+++ src/search.cpp
@@ -474,8 +474,9 @@ void Thread::search() {
           double bestMoveInstability = 1 + 1.7 * totBestMoveChanges / Threads.size();
           int complexity = mainThread->complexityAverage.value();
           double complexPosition = std::min(1.0 + (complexity - 261) / 1738.7, 1.5);
+          double fullTT = TT.hashfull() > 850 ? 0.5 : 1.0;
 
-          double totalTime = Time.optimum() * fallingEval * reduction * bestMoveInstability * complexPosition;
+          double totalTime = Time.optimum() * fallingEval * reduction * bestMoveInstability * complexPosition * fullTT;
 
           // Cap used time in case of a single legal move for a better viewer experience in tournaments
           // yielding correct scores and sufficiently fast moves.
